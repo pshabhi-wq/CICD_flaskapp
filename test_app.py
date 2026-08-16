06fff2a480f8171e8b599ec01655818f1aa89108
pshabhi-wq <psh.abhi@gmail.com>
2026-08-16 20:11:31 +0530

    Checkforsuccess

--- test_app.py
+++ test_app.py
@@ -27,7 +27,7 @@ def client():
 def test_home_page(client):
     """Test if home page loads correctly"""
     response = client.get('/')
-    assert 1==2
+    assert response.status_code == 200
     assert b"Test Student" in response.data
 
 
