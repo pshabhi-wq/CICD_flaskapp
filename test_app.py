1f0773c8cdd03d34ae9e11bb488e0ea8760cc2f1
pshabhi-wq <psh.abhi@gmail.com>
2026-08-16 20:07:13 +0530

    Test failure path

--- test_app.py
+++ test_app.py
@@ -27,7 +27,7 @@ def client():
 def test_home_page(client):
     """Test if home page loads correctly"""
     response = client.get('/')
-    assert response.status_code == 200
+    assert 1==2
     assert b"Test Student" in response.data
 
 
